a5a8c754747e119526e8e967ee6360e7865660b3
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2026-01-08 13:19:50 +0100

    updated the samples developer doc exported with DosInsight

---
+++ docs/Samples/toc/fMain.js
@@ -0,0 +1,85 @@
+
+window.DocInsight = window.DocInsight || {};
+window.DocInsight.toc = window.DocInsight.toc || {};
+window.DocInsight.toc["fMain"] =   [
+      {
+        id: "fMain",
+        title: "fMain",
+          href: "fMain.html",
+      },      {
+        id: "fMain-Classes",
+        title: "Classes",
+          children:   [
+      {
+        id: "fMain.TForm1",
+        title: "TForm1",
+          href: "fMain.TForm1.html",
+          children:   [
+      {
+        id: "fMain.TForm1-Methods",
+        title: "Methods",
+          children:   [
+      {
+        id: "fMain.TForm1.EllipsesEditButton1Click",
+        title: "EllipsesEditButton1Click",
+          href: "fMain.TForm1.EllipsesEditButton1Click.html",
+      },      {
+        id: "fMain.TForm1.LoadStylesList",
+        title: "LoadStylesList",
+          href: "fMain.TForm1.LoadStylesList.html",
+      },      {
+        id: "fMain.TForm1.ShowStyleInfos",
+        title: "ShowStyleInfos",
+          href: "fMain.TForm1.ShowStyleInfos.html",
+      },      {
+        id: "fMain.TForm1.edtStylesPathChange",
+        title: "edtStylesPathChange",
+          href: "fMain.TForm1.edtStylesPathChange.html",
+      },      {
+        id: "fMain.TForm1.lbStylesItemClick",
+        title: "lbStylesItemClick",
+          href: "fMain.TForm1.lbStylesItemClick.html",
+      }  ]
+,
+      },      {
+        id: "fMain.TForm1-Fields",
+        title: "Fields",
+          children:   [
+      {
+        id: "fMain.TForm1.EllipsesEditButton1",
+        title: "EllipsesEditButton1",
+          href: "fMain.TForm1.EllipsesEditButton1.html",
+      },      {
+        id: "fMain.TForm1.Splitter1",
+        title: "Splitter1",
+          href: "fMain.TForm1.Splitter1.html",
+      },      {
+        id: "fMain.TForm1.edtStylesPath",
+        title: "edtStylesPath",
+          href: "fMain.TForm1.edtStylesPath.html",
+      },      {
+        id: "fMain.TForm1.lbStyles",
+        title: "lbStyles",
+          href: "fMain.TForm1.lbStyles.html",
+      },      {
+        id: "fMain.TForm1.mmoInfos",
+        title: "mmoInfos",
+          href: "fMain.TForm1.mmoInfos.html",
+      }  ]
+,
+      }  ]
+,
+      }  ]
+,
+      },      {
+        id: "fMain-Variables",
+        title: "Variables",
+          children:   [
+      {
+        id: "fMain.Form1",
+        title: "Form1",
+          href: "fMain.Form1.html",
+      }  ]
+,
+      }  ]
+;
---
+++ docs/Samples/toc/toc-packages.js
@@ -0,0 +1,18 @@
+
+window.DocInsight = window.DocInsight || {};
+window.DocInsight.toc = window.DocInsight.toc || {};
+window.DocInsight.toc["toc-packages"] =   [
+      {
+        id: "index",
+        title: "FMX Styles Utils",
+          href: "index.html",
+      },      {
+        id: "DarkLightSample",
+        title: "DarkLightSample",
+          href: "DarkLightSample.html",
+      },      {
+        id: "GetStyleInfoSample",
+        title: "GetStyleInfoSample",
+          href: "GetStyleInfoSample.html",
+      }  ]
+;
